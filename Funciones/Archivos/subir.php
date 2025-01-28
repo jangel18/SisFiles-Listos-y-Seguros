b8409d359987e3f2f0fe9377815aa0b76e961017
jangel18 <18jangel18@gmail.com>
2025-01-28 14:24:21 -0600

    Funcion para subir archivos
    se habilita la funcion de subir archivos a la carpeta Public/Storage/files, ademas la informacion incluida la ruta de estos es guardada en una tabla de la base de datos

--- Funciones/Archivos/subir.php
+++ Funciones/Archivos/subir.php
@@ -1,1 +1,24 @@
+<?php
+
+session_start();
 include('../../Config/db.php');
+
+echo  "hola";
+if($_POST['subir']){
+    if(file_exists($_FILES['archivo']['tmp_name'])){
+        echo "exito";
+        $usuario_name=$_SESSION['user_name'];
+        $user_dir= '../../Public/Storage/files' .'/'. $usuario_name. '/';
+        echo $user_dir;
+        if(move_uploaded_file($_FILES['archivo']['tmp_name'],$user_dir . $_FILES['archivo']['name'] )){
+            $route=$user_dir . $_FILES['archivo']['name'];
+            $nombre_file= $_POST['nombre'];
+            $type="archivo";
+            $sql = $conn->query("INSERT INTO files (name,type,route) VALUES ('".$nombre_file."','".$type."','".$route."')");
+        }else{echo "error";}
+    }else{echo "error";}
+
+}else{
+    echo "no se a subido";
+}
+?>
--- Vistas/Usuario/paginauser.php
+++ Vistas/Usuario/paginauser.php
@@ -10,10 +10,11 @@
 <body>
     <h1>Hola <?php echo $_SESSION['user_name']; ?>, este es el inicio de sesion</h1>
     <a href="../../Funciones/Sesion/cerrar_sesion.php">Cerrar Sesión</a>
-    <form id="uploadForm">
+  <form method="POST" action="../Funciones/Archivos/subir.php" id="uploadForm" enctype="multipart/form-data">
     <label for="fileInput">Subir archivo:</label>
-    <input type="file" id="fileInput" multiple>
-    <button type="button" id="uploadButton">Subir</button>
+    <input type="text" name="nombre" placeholder="Nombrar archivo">
+    <input type="file" id="fileInput" name="archivo">
+    <input type="submit" id="uploadButton"name="subir">Subir</input>
   </form>
 
   <!-- Botón para crear carpetas -->
