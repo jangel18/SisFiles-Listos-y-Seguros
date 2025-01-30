6e49c2da9b6c30ca6a2d6597b953c541506313c9
jangel18 <18jangel18@gmail.com>
2025-01-30 17:45:46 -0600

    Creacion de carpetas y navegacion
    Ahora es posible  agregar y navegar entre carpetas

--- Funciones/Archivos/subir.php
+++ Funciones/Archivos/subir.php
@@ -9,13 +9,14 @@ if($_POST['subir']){
         echo "exito";
         $usuario_name=$_SESSION['user_name'];
         $file_size = $_FILES['archivo']['size'];
-        $user_dir= '../../Public/Storage/files' .'/'. $usuario_name. '/';
+        $user_dir= $_SESSION['ruta']. '/';
         echo $user_dir;
         if(move_uploaded_file($_FILES['archivo']['tmp_name'],$user_dir . $_FILES['archivo']['name'] )){
             $route=$user_dir . $_FILES['archivo']['name'];
             $nombre_file= $_POST['nombre'];
             $type="archivo";
             $sql = $conn->query("INSERT INTO files (name,type,size,route) VALUES ('".$nombre_file."','".$type."','".$file_size."','".$route."')");
+            header('Location: ../../Vistas/index.php');
         }else{echo "error";}
     }else{echo "error";}
 
