66bc122c89a6e138e05a2ec12e71d3e02e67ef76
jangel18 <18jangel18@gmail.com>
2025-02-03 14:55:41 -0600

    Update GestorArchivosUser.php

--- Vistas/Usuario/GestorArchivosUser.php
+++ Vistas/Usuario/GestorArchivosUser.php
@@ -18,7 +18,7 @@ echo $ruta;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="styles/icons/color_icons.css" />
-    <!--    fontawesome  para iconos  --> 
+    <!--    fontawesome  para iconos de carpetas,archivos y funcionalidades de estos  --> 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
     
